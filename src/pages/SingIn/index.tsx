cdb987becf097b2ab9c2b8c09dc5b6ae5f33e4e7
Júnior <valdecyborgesjr@hotmail.com>
2021-02-08 18:00:34 -0300

    Refatorando os componentes (Input/Button)

--- src/pages/SingIn/index.tsx
+++ src/pages/SingIn/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import Logo from '../../assets/logo.svg'
 import { Container, Content, Background } from './styles';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
 
 
 const SingIn: React.FC = () => {
@@ -11,10 +13,10 @@ const SingIn: React.FC = () => {
         <img src={Logo} alt="Logo"/>
         <form>
           <h1>Faça seu logon</h1>
-          <input placeholder="E-mail"/>
-          <input placeholder="Senha" type="password"/>
+          <Input Icon={FiMail} name="email" placeholder="E-mail"/>
+          <Input Icon={FiLock} name="password" placeholder="Senha" type="password"/>
 
-          <button type="submit">Entrar</button>
+          <Button type="submit">Entrar</Button>
 
           <a href="adf">Esqueci minha senha</a>
         </form>
